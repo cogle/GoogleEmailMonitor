d2a832fa9d2d3b95906191ab3c1932ea3c10cd03
Christopher Ogle <thecogle@gmail.com>
2016-05-21 14:57:57 -0500

    Restructed into OOP format

--- Monitor.py
+++ Monitor.py
@@ -1,5 +1,5 @@
 #!./bin/python3.4
-
+from __future__ import print_function
 import httplib2
 import os
 
@@ -7,3 +7,47 @@ from apiclient import discovery
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
+
+
+class EmailMonitor:
+    service = None
+
+    def get_credentials(self):
+        """
+        Gets credentials so that one may access a particular google account
+        """
+        credential_dir =   os.environ['GOOGLE_CREDENTIALS_DIR']
+        credential_path = os.path.join(credential_dir,
+                                       os.environ['GOOGLE_CREDENTIALS_FILE'])
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            print("Invalid credentials provided")
+            os.exit(1)
+        return credentials
+
+    def get_service(self):
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        self.service = discovery.build('gmail', 'v1', http=http)
+
+    def run(self):
+        self.get_credentials()
+        self.get_service()
+        if not self.service:
+            print("Unable to correctly start service")
+            os.exit(1)
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    monitor = EmailMonitor()
+    monitor.run()
+
+
+if __name__ == '__main__':
+    main()
